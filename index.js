4cb1aa69a98e983ccb9b7491ebc07a5dfd38dde4
kamo312 <kamogelomalema1@gmail.com>
2025-04-23 11:15:50 -0400

    added functionality to compare dice and determine a winner

--- index.js
+++ index.js
@@ -15,3 +15,10 @@ console.log(randomNumber2);
 document.querySelector("img.img1").setAttribute("src", `./images/dice${randomNumber1}.png`);
 document.querySelector("img.img2").setAttribute("src", `./images/dice${randomNumber2}.png`);
 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerText = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerText = "Player 2 Wins!";
+} else {
+    document.querySelector("h1").innerText = "Draw!";
+}
